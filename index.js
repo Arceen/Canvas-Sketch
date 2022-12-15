a88b2b19b3405171424249be59d75e2eebd855c6
Arceen <niloyman01@gmail.com>
2022-12-15 16:20:23 +0600

    1 file modified

--- index.js
+++ index.js
@@ -1,45 +1,43 @@
 const canvasSketch = require('canvas-sketch');
 const math = require('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
 
 const settings = {
   dimensions: [1080, 1080],
+  animate: true,
 };
 
-const sketch = () => {
+const sketch = ({ context, width, height }) => {
+  const num = 20;
+  const degree = -30;
+  const rects = [];
+  for (let i = 0; i < num; i++) {
+    rects.push({
+      x: random.range(0, width),
+      y: random.range(0, height),
+      w: random.range(200, 600),
+      h: random.range(40, 200),
+    });
+  }
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
     context.fillStyle = 'black';
-    let x = 0.5 * width;
-    let y = 0.5 * height;
-    let w = 0.6 * width;
-    let h = 0.1 * height;
-
-    // let radius = 200;
-    // context.translate(-0.5 * w, -0.5 * h);
-
-    // context.strokeRect(x - 0.5 * w, y - 0.5 * h, w, h);
-
-    // context.beginPath();
-    // context.moveTo(0, 0);
-    // context.lineTo(w, 0);
-    // context.lineTo(w, h);
-    // context.lineTo(0, h);
-    // context.closePath();
-    // context.stroke();
-    context.save();
-    context.strokeStyle = 'blue';
-    context.translate(x, y);
-    drawSkewedRect({ context: context, w: w, h: h, degree: 30 });
-    context.restore();
-    let angle = 30;
+    rects.forEach(({ x, y, w, h }) => {
+      context.save();
+      context.strokeStyle = 'blue';
+      context.translate(x, y);
+      drawSkewedRect({ context: context, w: w, h: h, degree: degree });
+      context.restore();
+    });
   };
 };
 
 const drawSkewedRect = ({ context, w = 600, h = 200, degree = -45 }) => {
-  let rx = w * Math.cos(math.degToRad(degree));
-  let ry = w * Math.sin(math.degToRad(degree));
+  const rx = w * Math.cos(math.degToRad(degree));
+  const ry = w * Math.sin(math.degToRad(degree));
 
+  context.save();
   context.translate(rx * -0.5, (ry + h) * -0.5);
   context.beginPath();
   context.moveTo(0, 0);
@@ -49,6 +47,7 @@ const drawSkewedRect = ({ context, w = 600, h = 200, degree = -45 }) => {
 
   context.closePath();
   context.stroke();
+  context.restore();
 };
 
 canvasSketch(sketch, settings);
