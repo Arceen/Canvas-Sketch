11cc48ecc5785728dff0883213496fe5284fe8ce
Arceen <niloyman01@gmail.com>
2022-12-15 16:56:45 +0600

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const canvasSketch = require('canvas-sketch');
 const math = require('canvas-sketch-util/math');
 const random = require('canvas-sketch-util/random');
+const Color = require('canvas-sketch-util/color');
 const risoColors = require('riso-colors');
 
 const settings = {
@@ -9,25 +10,27 @@ const settings = {
 };
 
 const sketch = ({ context, width, height }) => {
-  const num = 20;
+  const num = 40;
   const degree = -30;
   const rectColors = [
     random.pick(risoColors),
     random.pick(risoColors),
     random.pick(risoColors),
+
+    random.pick(risoColors),
   ];
   const rects = [];
-  console.log(risoColors[10]);
   for (let i = 0; i < num; i++) {
     rects.push({
       x: random.range(0, width),
       y: random.range(0, height),
-      w: random.range(200, 600),
+      w: random.range(300, width),
       h: random.range(40, 200),
       // stroke: 'black',
       // fill: `rgba(${random.range(0,256)}, ${random.range(0,256)} , ${random.range(0,256)}, ${Math.random()})`,
       stroke: random.pick(rectColors).hex,
       fill: random.pick(rectColors).hex,
+      blend: Math.random() > 0.3 ? 'overlay' : 'source-over',
     });
   }
   const bgColor = random.pick(risoColors).hex;
@@ -35,17 +38,27 @@ const sketch = ({ context, width, height }) => {
     context.fillStyle = bgColor;
     context.fillRect(0, 0, width, height);
     context.fillStyle = 'black';
-    rects.forEach(({ x, y, w, h, stroke, fill }) => {
+    rects.forEach(({ x, y, w, h, stroke, fill, blend }) => {
       context.save();
       context.translate(x, y);
-
+      context.globalCompositeOperation = blend;
       context.strokeStyle = stroke;
       context.fillStyle = fill;
       context.lineWidth = 10;
       drawSkewedRect({ context: context, w: w, h: h, degree: degree });
 
-      context.stroke();
+      context.shadowColor = Color.style(Color.offsetHSL(fill, 0, 0, -20).rgba);
+      context.shadowOffsetX = -10;
+      context.shadowOffsetY = 20;
       context.fill();
+
+      context.shadowColor = null;
+      context.stroke();
+      context.globalCompositeOperation = 'source-over';
+      context.lineWidth = 2;
+      context.strokeStyle = 'black';
+      context.stroke();
+
       context.restore();
     });
   };
